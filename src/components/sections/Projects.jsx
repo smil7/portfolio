2a7815bf156d8d70f543de88d3de78b204be29e6
Smily Os <osama2231@hotmail.com>
2025-04-24 16:03:43 -0400

    Add a new project

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -95,6 +95,34 @@ export const Projects = () => {
                             </a>
                         </div>
                     </div>
+                    <div className="p-6 rounded-xl border border-white/20 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.2)] transition">
+                        <h3 className="text-xl font-bold mb-2">Lexical & Syntax Analyzer</h3>
+                        <p className="text-gray-400 mb-4">
+                            Developed a C++ system that can read and analyze a 
+                            new programming language by using lexical 
+                            and syntax analyzer techniques
+                        </p>
+                        <div className="flex flex-wrap gap-2 mb-4">
+                            {["C++"].map((tech, key) => (
+                                <span 
+                                    key={key}
+                                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
+                                                hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.1)] transition-all
+                                ">
+                                    {tech}
+                                </span>
+                            ))}
+                        </div>
+                        <div className="flex justify-between items-center">
+                            <a 
+                                href="https://github.com/smil7/lexical-syntax-analyzer"
+                                className="text-blue-400 hover:text-blue-300 transition-colors my-4"    
+                            > 
+                                {" "}
+                                View Project {"->"} 
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
